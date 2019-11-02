65ce00ada0c1f88870b122f3d1e10ef47942a1d3
Jessica-Morgan <jess-morgan1@outlook.com>
2019-11-03 12:17:38 +1300

    added scripts and dependencies

--- client/index.js
+++ client/index.js
@@ -1,21 +1,31 @@
 
 import React from 'react'
-import {render} from 'react-dom'
+import ReactDOM from 'react-dom'
 import {Provider} from 'react-redux'
-import {createStore} from 'redux'
+import {createStore, applyMiddleware, compose} from 'redux'
+import thunkMiddleware from 'redux-thunk'
+import {BrowserRouter as Router} from 'react-router-dom'
 
-import reducers from './reducers'
 import App from './components/App'
+import reducers from './reducers'
 
-const store = createStore(reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-)
+let store = createStore(reducers, compose(
+  applyMiddleware(thunkMiddleware),
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+))
 
 document.addEventListener('DOMContentLoaded', () => {
-  render(
+  render()
+  store.subscribe(render)
+})
+
+function render () {
+  ReactDOM.render(
     <Provider store={store}>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </Provider>,
     document.getElementById('app')
   )
-})
+}
