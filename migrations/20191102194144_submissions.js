d34782dd7f2e83c6e7a691902964c9cf618d824c
Jessica-Morgan <jess-morgan1@outlook.com>
2019-11-02 19:48:36 +1300

    submission.email references members.email

--- migrations/20191102194144_submissions.js
+++ migrations/20191102194144_submissions.js
@@ -1,7 +1,7 @@
 exports.up = function (knex) {
   return knex.schema.createTable('submissions', table => {
     table.increments('id').primary()
-    table.string('email').unique()
+    table.string('email').unique().references('members.email')
     table.string('topic')
     table.string('synopsis')
     table.timestamp('submission_time')
