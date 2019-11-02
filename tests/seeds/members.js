48ee0c5dd15821fe4e73e6a5edb9db50c3ab0423
Jessica-Morgan <jess-morgan1@outlook.com>
2019-11-03 11:05:48 +1300

    setup for testing

--- tests/seeds/members.js
+++ tests/seeds/members.js
@@ -1,8 +1,6 @@
 exports.seed = function (knex) {
-  // Deletes ALL existing entries
   return knex('members').del()
     .then(function () {
-      // Inserts seed entries
       return knex('members').insert([
         { id: 1, email: 'testuser@test.com', hash: '$argon2id$v=19$m=8,t=2,p=1$WjepFSlq8cfNuFZ0fUZlsg$Hdr+LA6oGQthW4pX0BYUUV0sqWfDU1KblbsL+yJEVJs' }
       ])
