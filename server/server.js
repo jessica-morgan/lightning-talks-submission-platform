120d560238fedaf6d592d307abcf3fe6ff2d1e55
jess morgan <jess-morgan1@outlook.com>
2019-11-02 17:35:23 +1300

    added auth routes and cors to server.js

--- server/server.js
+++ server/server.js
@@ -1,10 +1,21 @@
-
 const path = require('path')
 const express = require('express')
-
 const server = express()
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, '../public')))
+server.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
+
+const authRoutes = require('./routes/auth')
+
+server.use('/api/v1/auth', authRoutes)
+
+server.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+})
 
 module.exports = server
