4954e995fac57a63903e4724add2ccb5fa1e6492
jess morgan <jess-morgan1@outlook.com>
2019-11-03 14:13:08 +1300

    registration component created

---
+++ client/components/Register.jsx
@@ -0,0 +1,78 @@
+import React from 'react'
+import { Redirect, Link, withRouter } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { register } from '../actions/auth'
+import Grid from '@material-ui/core/Grid'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
+
+class Register extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      email: '',
+      password: ''
+    }
+  }
+
+  handleChange = (e) => {
+    const { name, value } = e.target
+    this.setState({
+      [name]: value
+    })
+  }
+
+  handleSubmit = (e) => {
+    const member = this.state
+    this.props.dispatch(register(member))
+    e.preventDefault()
+  }
+
+  render () {
+    if (this.props.loggedIn) {
+      return <Redirect to='/home' />
+    }
+
+    const { email, password } = this.state
+    return (
+      <div>
+        <form>
+          <Grid container direction="column" justify="center" alignItems="center">
+            <TextField
+              label="email"
+              margin="normal"
+              name="email"
+              variant="outlined"
+              onChange={this.handleChange}></TextField>
+            <TextField
+              label="password"
+              margin="normal"
+              name="password"
+              variant="outlined"
+              type="password"
+              onChange={this.handleChange} ></TextField>
+            <Link to='/' onClick={this.handleSubmit}><Button variant="contained" color="primary" >
+              Register
+            </Button>
+            </Link>
+            <h3>Already have an account? </h3>
+            <Link to='/'><Button variant="contained" color="primary" >
+              Login
+            </Button>
+            </Link>
+          </Grid>
+        </form>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    loggedIn: state.auth.loggedIn,
+    error: state.auth.error,
+    registered: state.auth.newRegistration
+  }
+}
+
+export default withRouter(connect(mapStateToProps)(Register))
